eeca94c2242381f606671a39884985a14efd9b46
Vu Nguyen <vunguyen2712@gmail.com>
2015-04-19 02:39:47 -0700

    working Join (VuNguyen)

--- js/events.js
+++ js/events.js
@@ -11,6 +11,17 @@ $(function(){
 
     });
 
+    $('#submitButtonId').click(function(e) {
+        e.preventDefault();
+        
+        $('#joinEventModal').on('shown.bs.modal', function () {
+            
+        })
+        
+
+
+    });
+
     $('#joinButtonId').click(function(e) {
         $("#joinIid").removeClass("fa fa-chevron-up fa-5x");
         $("#joinIid").addClass("fa fa-hand-o-down fa-5x");
