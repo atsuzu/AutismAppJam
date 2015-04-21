cf11f5ddb6859373f4527035a3dc5266408713e1
SargisPlusPlus <sdudakly@uci.edu>
2015-04-20 17:36:35 -0700

    replaced html with php

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+
+  <title>AutismConnect</title>
+  <meta name="description" content="My Parse App">
+  <meta name="viewport" content="width=device-width">
+  <!--<link rel='stylesheet' type='text/css' href='stylesheet.css'/>-->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+   <!-- Custom styles for this template -->
+  <link href="grid.css" rel="stylesheet">
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="https://www.parsecdn.com/js/parse-1.2.18.min.js"></script>
+
+  <link rel="stylesheet" href="css/style.css">
+  <script type = "text/javascript" src="js/javascript.js"></script>
+  <link rel="stylesheet" href="css/animeButton.css">
+
+</head>
+
+<body>
+  <div class="container">
+  <div class="login-container">
+            <div id="output"></div>
+            <div class="avatar"></div>
+            <div class="form-box">
+                <form action="" method="">
+                    <input name="user" type="text" placeholder="username">
+                    <input name="pass" type="password" placeholder="password">
+                    <button class="btn login m-progress btn-primary" type="submit"><span id="loginText">Login</span></button>
+                </form>
+            </div>
+        </div>
+  </div>
+
+</body>
+
+</html>
